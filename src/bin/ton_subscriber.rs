f5f77251d336b05ff7a37326325ff4c86d30f3e1
Dejan Cabrilo <dcabrilo@gmail.com>
2025-07-12 16:31:09 +0200

    Remove unneeded cache usage

--- src/bin/ton_subscriber.rs
+++ src/bin/ton_subscriber.rs
@@ -5,6 +5,7 @@ use relayer_base::queue::Queue;
 use relayer_base::subscriber::Subscriber;
 use relayer_base::utils::{setup_heartbeat, setup_logging};
 use tokio::signal::unix::{signal, SignalKind};
+use tokio::task::JoinHandle;
 use ton::config::TONConfig;
 use ton::subscriber::TONSubscriber;
 use tonlib_core::TonAddress;
@@ -21,18 +22,10 @@ async fn main() -> anyhow::Result<()> {
     let postgres_db = PostgresDB::new(&config.common_config.postgres_url).await?;
 
     let ton_gateway = config.ton_gateway;
-    let account = TonAddress::from_base64_url(ton_gateway.as_str())?;
-
-    let ton_subscriber = TONSubscriber::new(
-        config.ton_rpc,
-        config.ton_api_key,
-        postgres_db,
-        "default".to_string(),
-        config.common_config.chain_name,
-    )
-    .await?;
-
-    let mut subscriber = Subscriber::new(ton_subscriber);
+    let ton_gas_service = config.ton_gas_service;
+    let gateway_account = TonAddress::from_base64_url(ton_gateway.as_str())?;
+    let gas_service_account = TonAddress::from_base64_url(ton_gas_service.as_str())?;
+    
     let mut sigint = signal(SignalKind::interrupt())?;
     let mut sigterm = signal(SignalKind::terminate())?;
 
@@ -41,10 +34,35 @@ async fn main() -> anyhow::Result<()> {
 
     setup_heartbeat("heartbeat:subscriber".to_owned(), redis_pool);
 
+
+    let mut handles: Vec<JoinHandle<()>> = vec![];
+
+    for acct in vec![gateway_account, gas_service_account] {
+        let ton_sub = TONSubscriber::new(
+            config.ton_rpc.clone(),
+            config.ton_api_key.clone(),
+            postgres_db.clone(),
+            acct.to_string(),
+            config.common_config.chain_name.clone(),
+        )
+            .await?;
+        let mut sub = Subscriber::new(ton_sub);
+        let queue_clone = events_queue.clone();
+        let handle = tokio::spawn(async move {
+            sub.run(acct, queue_clone).await;
+        });
+        handles.push(handle);
+
+    }
+
+
     tokio::select! {
         _ = sigint.recv()  => {},
         _ = sigterm.recv() => {},
-        _ = subscriber.run(account, events_queue.clone()) => {},
+    }
+
+    for handle in handles {
+        handle.abort();
     }
 
     events_queue.close().await;

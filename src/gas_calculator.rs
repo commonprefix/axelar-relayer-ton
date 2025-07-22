35a52d771b78540b21174007e567092f7b024d3f
Dejan Cabrilo <dcabrilo@gmail.com>
2025-07-22 17:54:44 +0200

    Change gas calculation to account for an issue in toncenter api

--- src/gas_calculator.rs
+++ src/gas_calculator.rs
@@ -1,20 +1,8 @@
-/*!
-
-Why we see empty balances?
-
- * **Every** transaction _always_ gets its `AccountStateBefore/After` fields primed with just the hash.
- * The code then does a lookup in `account_states` to fill in balance, status, etc—but only for those hashes that _actually exist_ in the table.
- * If a particular state‐hash was never recorded in `account_states` (for example, the indexer didn’t snapshot every single intermediate state, or that account had no on‐chain state change so no row was inserted), **no** row is returned for that hash.
- * In that case the `AccountStateBefore/After` pointer remains the bare struct with only the hash, and all the other fields (including `balance`) stay nil → you get an empty JSON object (no `balance`) for those slots.
-
-*/
-
 use crate::error::GasError;
-use crate::ton_constants::{OP_MESSAGE_APPROVE, OP_NULLIFY_IF_APPROVED};
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::ops::AddAssign;
 use std::str::FromStr;
-use ton_types::ton_types::Transaction;
+use ton_types::ton_types::{Transaction, TransactionMessage};
 use tonlib_core::TonAddress;
 
 #[derive(Default)]
@@ -22,18 +10,52 @@ pub struct GasCalculator {
     our_addresses: Vec<TonAddress>,
 }
 
+fn extract_fwd_fee(msg: &TransactionMessage) -> i128 {
+    let fwd_fee = msg
+        .fwd_fee
+        .clone()
+        .unwrap_or("0".to_string())
+        .parse::<i128>()
+        .unwrap_or(0);
+    fwd_fee
+}
+
+fn add_cost(balances: &mut HashMap<TonAddress, i128>, account: TonAddress, cost: i128) {
+    balances.entry(account).or_insert(0).add_assign(cost);
+}
+
+fn is_our_transaction(balances: &mut HashMap<TonAddress, i128>, tx: &&Transaction) -> bool {
+    balances.contains_key(&tx.account)
+}
+
+fn us_receiving(
+    balances: &mut HashMap<TonAddress, i128>,
+    tx: &&Transaction,
+    dest: &TonAddress,
+) -> bool {
+    !balances.contains_key(&tx.account) && balances.contains_key(&dest)
+}
+
+fn us_sending(
+    balances: &mut HashMap<TonAddress, i128>,
+    tx: &&Transaction,
+    dest: &TonAddress,
+) -> bool {
+    balances.contains_key(&tx.account) && !balances.contains_key(&dest)
+}
+
+fn extract_msg_value(msg: TransactionMessage) -> i128 {
+    let value = msg
+        .value
+        .unwrap_or("0".to_string())
+        .parse::<i128>()
+        .unwrap_or(0);
+    value
+}
+
 impl GasCalculator {
     pub fn new(our_addresses: Vec<TonAddress>) -> Self {
         Self { our_addresses }
-    }
-
-    fn load_address(&self, addr: &Option<TonAddress>) -> Result<Option<TonAddress>, GasError> {
-        if let Some(s) = addr {
-            let addr = s.clone();
-            Ok(Some(addr).filter(|a| self.our_addresses.contains(a)))
-        } else {
-            Ok(None)
-        }
     }
 
     pub fn calc_message_gas_native_gas_refunded(
@@ -44,7 +66,7 @@ impl GasCalculator {
             return Ok(0);
         }
 
-        let balance_diff = self.balance_diff(transactions)?;
+        let balance_diff = self.cost(transactions)?;
         let refund = i128::from_str(
             &transactions[2].out_msgs[0]
                 .value
@@ -57,44 +79,32 @@ impl GasCalculator {
         Ok(if total > 0 { total as u64 } else { 0 })
     }
 
-    fn balance_diff(&self, transactions: &[Transaction]) -> Result<i128, GasError> {
-        let mut dynamic_addresses: HashSet<TonAddress> =
-            self.our_addresses.iter().cloned().collect();
-        let mut balances: HashMap<TonAddress, i128> = HashMap::new();
+    fn cost(&self, transactions: &[Transaction]) -> Result<i128, GasError> {
+        let mut balances: HashMap<TonAddress, i128> = self
+            .our_addresses
+            .iter()
+            .cloned()
+            .map(|addr| (addr, 0))
+            .collect();
 
         for tx in transactions {
-            if (self.load_address(&Some(tx.account.clone()))?).is_some() {
-                for msg in &tx.out_msgs {
-                    if let Some(op) = &msg.opcode {
-                        if *op == OP_MESSAGE_APPROVE || *op == OP_NULLIFY_IF_APPROVED {
-                            if let Some(dest) = &msg.destination {
-                                dynamic_addresses.insert(dest.clone());
-                            }
-                        }
+            if is_our_transaction(&mut balances, &tx) {
+                add_cost(&mut balances, tx.account.clone(), tx.total_fees as i128);
+            }
+            for msg in tx.out_msgs.clone() {
+                if is_our_transaction(&mut balances, &tx) {
+                    add_cost(&mut balances, tx.account.clone(), extract_fwd_fee(&msg));
+                }
+                if let Some(dest) = msg.destination.clone() {
+                    let value = extract_msg_value(msg);
+                    // Us sending to someone
+                    if us_sending(&mut balances, &tx, &dest) {
+                        add_cost(&mut balances, tx.account.clone(), value);
+                    }
+                    if us_receiving(&mut balances, &tx, &dest) {
+                        add_cost(&mut balances, dest.clone(), 0 - value);
                     }
                 }
-            }
-
-            let balance_before = i128::from_str(
-                &tx.account_state_before
-                    .balance
-                    .clone()
-                    .unwrap_or("0".to_string()),
-            )
-            .unwrap_or(0);
-            let balance_after = i128::from_str(
-                &tx.account_state_after
-                    .balance
-                    .clone()
-                    .unwrap_or("0".to_string()),
-            )
-            .unwrap_or(0);
-            let balance = balance_before - balance_after;
-            if dynamic_addresses.contains(&tx.account) {
-                balances
-                    .entry(tx.account.clone())
-                    .or_insert(0)
-                    .add_assign(balance);
             }
         }
 
@@ -103,7 +113,7 @@ impl GasCalculator {
     }
 
     pub fn calc_message_gas(&self, transactions: &[Transaction]) -> Result<u64, GasError> {
-        let total = self.balance_diff(transactions)?;
+        let total = self.cost(transactions)?;
         Ok(if total > 0 { total as u64 } else { 0 })
     }
 }
@@ -119,15 +129,18 @@ mod tests {
         let traces = fixture_traces();
 
         let our_addresses = vec![
-            TonAddress::from_base64_url("EQCQPVhDBzLBwIlt8MtDhPwIrANfNH2ZQnX0cSvhCD4DlThU")
+            TonAddress::from_base64_url("0QCQPVhDBzLBwIlt8MtDhPwIrANfNH2ZQnX0cSvhCD4Dld4b")
                 .unwrap(),
-            TonAddress::from_base64_url("EQBcfOiB4SF73vEFm1icuf3oqaFHj1bNQgxvwHKkxAiIjxLZ")
+            TonAddress::from_base64_url("kQAAGUqtjkIr7fQ_7nRtbZKdNp26slRopp1RNwbqaXi2OnXH")
                 .unwrap(),
         ];
 
         let calc = GasCalculator::new(our_addresses);
         let amount = calc.calc_message_gas(&traces[2].transactions);
-        assert_eq!(amount.unwrap(), 27244157);
+        // We should equal: https://testnet.tonviewer.com/transaction/6c167d523f4a1ceecdcfacb477f39a6fb622e2174e7f70f4308009b6e51cf66f
+        // Sum of network fees for A and B, plus sum of outgoing values from B to C, minus
+        // incoming value from C to B
+        assert_eq!(amount.unwrap(), 48524353);
     }
 
     #[test]
@@ -178,7 +191,8 @@ mod tests {
 
         let calc = GasCalculator::new(our_addresses);
         let amount = calc.calc_message_gas(&traces[12].transactions);
-        assert_eq!(amount.unwrap(), 42744830);
+        // https://testnet.tonviewer.com/transaction/943579cdd5d9a87ce149a2b08b019fdad1747fbc07c6058f0b3c8bfb239be80e?section=valueFlow
+        assert_eq!(amount.unwrap(), 48724830);
     }
 
     #[test]
@@ -194,7 +208,7 @@ mod tests {
 
         let calc = GasCalculator::new(our_addresses);
         let amount = calc.calc_message_gas(&traces[13].transactions);
-        assert_eq!(amount.unwrap(), 42628574);
+        assert_eq!(amount.unwrap(), 48622974);
     }
 
     #[test]
@@ -212,7 +226,25 @@ mod tests {
         let amount = calc.calc_message_gas(&traces[11].transactions);
 
         // https://testnet.tonviewer.com/transaction/3594522d1f0b4ec694558538e1f4b701acae9f7c9ee282f5c73b08ca18cb322c?section=valueFlow
-        // We should have the same number as A, B and C
-        assert_eq!(amount.unwrap(), 36656408 + 4271633);
+        // We should have the same as:
+        // A networkfees + B network fees + B->C value - C->B value + B -> D value
+        assert_eq!(amount.unwrap(), 40928034);
+    }
+
+    #[test]
+    fn test_gas_approved_c() {
+        let traces = fixture_traces();
+
+        let our_addresses = vec![
+            TonAddress::from_base64_url("0QCQPVhDBzLBwIlt8MtDhPwIrANfNH2ZQnX0cSvhCD4Dld4b")
+                .unwrap(),
+            TonAddress::from_base64_url("kQBLABicwV3LJN07SVHHnC4HB1cAKkerrNODHzrzwZmsKqmO")
+                .unwrap(),
+        ];
+
+        let calc = GasCalculator::new(our_addresses);
+        let amount = calc.calc_message_gas(&traces[16].transactions);
+        // https://testnet.tonviewer.com/transaction/64d36666fde95c4022ddda652db2047cade337ab9423496d727ab325d33fd230?section=valueFlow
+        assert_eq!(amount.unwrap(), 48647601);
     }
 }

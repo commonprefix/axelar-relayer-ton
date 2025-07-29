3a78c20cd446b2e2448ed67646e3953bccdfcbef
Dejan Cabrilo <dcabrilo@gmail.com>
2025-07-29 17:07:45 +0200

    cargo fmt

--- src/includer.rs
+++ src/includer.rs
@@ -5,13 +5,13 @@ use crate::gas_estimator::TONGasEstimator;
 use crate::high_load_query_id_db_wrapper::HighLoadQueryIdDbWrapper;
 use crate::lock_manager::RedisLockManager;
 use crate::wallet_manager::WalletManager;
+use relayer_base::utils::ThreadSafe;
 use relayer_base::{
     database::Database, error::BroadcasterError, gmp_api::GmpApiTrait, includer::Includer,
     payload_cache::PayloadCache, queue::Queue,
 };
 use std::sync::Arc;
 use tonlib_core::TonAddress;
-use relayer_base::utils::ThreadSafe;
 
 pub struct TONIncluder {}
 

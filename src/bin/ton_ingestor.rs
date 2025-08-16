c0e471debfef32e42ca86b89857643e59fb736b7
Dejan Cabrilo <dcabrilo@gmail.com>
2025-08-16 13:15:34 +0200

    feat: logging_ctx_cache implementation
    Logging context cache is used to store opentelemetry context in redis
    cache, so we can connect message_id across service journeys.

--- src/bin/ton_ingestor.rs
+++ src/bin/ton_ingestor.rs
@@ -4,6 +4,7 @@ use relayer_base::database::PostgresDB;
 use relayer_base::gmp_api;
 use relayer_base::ingestor::Ingestor;
 use relayer_base::logging::setup_logging;
+use relayer_base::logging_ctx_cache::RedisLoggingCtxCache;
 use relayer_base::price_view::PriceView;
 use relayer_base::queue::Queue;
 use relayer_base::redis::connection_manager;
@@ -57,10 +58,11 @@ async fn main() -> anyhow::Result<()> {
 
     let ton_trace_model = PgTONTraceModel::new(pg_pool.clone());
     let ton_ingestor = TONIngestor::new(parser, ton_trace_model);
-    let ingestor = Ingestor::new(gmp_api, ton_ingestor);
-
     let redis_client = redis::Client::open(config.common_config.redis_server.clone())?;
     let redis_conn = connection_manager(redis_client, None, None, None).await?;
+
+    let logging_ctx_cache = RedisLoggingCtxCache::new(redis_conn.clone());
+    let ingestor = Ingestor::new(gmp_api, ton_ingestor, Arc::new(logging_ctx_cache));
 
     setup_heartbeat("heartbeat:ingestor".to_owned(), redis_conn);
 
